22a199a3904fe85d82b29122990c314067bcd260
danidanidani11 <dssnuandddani@gmail.com>
2025-07-11 21:45:26 +0330

    Update main.py

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ import os
 from telebot import types
 
 bot = telebot.TeleBot('7459857250:AAHpb_NliuOiM7-cTmFSrospKdoKMnAFiew')
-‏admin_id = 5542927340
-‏channel = 'bagha_game'
-‏tron_address = 'TJ4xrwKJzKjk6FgKfuuqwah3Az5Ur22kJb'
+admin_id = 5542927340
+channel = 'bagha_game'
+tron_address = 'TJ4xrwKJzKjk6FgKfuuqwah3Az5Ur22kJb'
 
 questions = [
     {
